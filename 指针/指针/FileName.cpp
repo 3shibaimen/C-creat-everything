1335ff4ec17cb80874bee5b94d4192ea6ba0b83e
zhai <2412605842@qq.com>
2024-01-27 18:59:21 +0800

    提交代码

---
+++ 指针/指针/FileName.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	int** ppa = &pa;     //ppa就是二级指针
+//	int*** pppa = &ppa;
+//	printf("%d\n", **ppa);
+//	printf("%d\n", ***pppa);
+//	return 0;
+//}
+

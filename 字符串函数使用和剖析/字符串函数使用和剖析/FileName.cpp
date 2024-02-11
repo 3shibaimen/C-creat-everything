b3c94771e7dc9aa451793f4206f1152d1f347657
zhai <2412605842@qq.com>
2024-02-11 10:33:47 +0800

    提交代码

---
+++ 字符串函数使用和剖析/字符串函数使用和剖析/FileName.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+
+
+//int my_strcmp(const char* str1, const char* str2)
+//{
+//	assert(str1 && str2);
+//		while (*str1 == *str2)
+//		{
+//			if (*str1 == '\0')
+//			{
+//				return 0;
+//			}
+//			str1++;
+//			str2++;
+//	     }
+//	if (*str1 > *str2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return -1;
+//	}
+//}
+//
+//int main()
+//{
+//	char str1[] = "abc";    //str1存放a的地址
+//	char* p1 = str1;
+//	char str2[] = "rgf";     //str2存放r的地址
+//	char* p2 = str2;
+//	int ret = my_strcmp(p1, p2);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
+
+
+
+
